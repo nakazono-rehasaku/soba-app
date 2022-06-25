e132bbc7ba8cccc04d04d54c3e15fe60c5552fec
nakazono1011 <syousanitiosi1011@yahoo.co.jp>
2022-06-25 18:30:25 +0900

    introduce material-ui

--- front/front-next/src/pages/index.tsx
+++ front/front-next/src/pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next';
+import Button from '@mui/material/Button';
+import StickyHeadTable from 'src/components/Table';
 
 const Home: NextPage = () => {
+  const onClickHandler = () => {
+    console.log('you are so great!!');
+  };
+
   return (
     <>
       <h1>Hello World</h1>
+      <Button variant='contained' onClick={onClickHandler}>
+        Click Me
+      </Button>
+      <br />
+      <br />
+      <br />
+      <StickyHeadTable />
     </>
   );
 };
